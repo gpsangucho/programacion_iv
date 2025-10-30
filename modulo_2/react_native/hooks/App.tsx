0a20a58ba95e4a24d50d09fe19bf73a2a44396c8
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-29 19:46:17 -0500

    Clase 29-10-2025

---
+++ modulo_2/react_native/hooks/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+  <AppNavigator />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
