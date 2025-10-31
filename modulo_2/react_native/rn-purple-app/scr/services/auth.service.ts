e1b3b1faa25270a222ce0f6ec04840f7d91eae3c
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-30 21:51:03 -0500

    Clase 30-oct-2025

---
+++ modulo_2/react_native/rn-purple-app/scr/services/auth.service.ts
@@ -0,0 +1,37 @@
+import api from "./api";
+import { authStore } from "../store/auth";
+
+// API pública: https://reqres.in
+//const BASE = "https://reqres.in/api";
+const BASE = "http://localhost:8000/api/auth";
+/*
+export async function login(email: string, password: string) {
+  const { data } = await api.post(`${BASE}/login`, { email, password });
+  authStore.set({ token: data.token, email });
+  return data;
+}
+  */
+export async function login(email: string, password: string) {
+    const { data } = await api.post(`${BASE}/login`, { username: email, password });
+    authStore.set({ token: data.token, email });
+    return data;
+  }
+
+  export async function register(email: string, password: string) {
+    const { data } = await api.post(`${BASE}/register`, { username: email, email, password });
+    // Opcional: guardar token si devuelve
+    authStore.set({ token: data.token ?? null, email });
+    return data;
+  }
+  /*
+export async function register(email: string, password: string) {
+  const { data } = await api.post(`${BASE}/register`, { email, password });
+  // Opcional: guardar token si devuelve
+  authStore.set({ token: data.token ?? null, email });
+  return data;
+}
+  */
+
+export function logout() {
+  authStore.clear();
+}
