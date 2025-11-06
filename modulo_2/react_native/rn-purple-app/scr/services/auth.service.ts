e17b8cb30e225f85e8465f69be7ba5e51871ffd2
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-11-05 20:14:31 -0500

    class 5-nov-2025

--- modulo_2/react_native/rn-purple-app/scr/services/auth.service.ts
+++ modulo_2/react_native/rn-purple-app/scr/services/auth.service.ts
@@ -1,9 +1,14 @@
 import api from "./api";
 import { authStore } from "../store/auth";
+import { Platform } from "react-native";
 
 // API pública: https://reqres.in
 //const BASE = "https://reqres.in/api";
-const BASE = "http://localhost:8000/api/auth";
+//const BASE = "http://localhost:8000/api/auth";
+const host = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+
+//La siguiente linea debe sustituirse con la url de nuestro backend.
+export const BASE = 'http://${host}:8000/api/auth';
 /*
 export async function login(email: string, password: string) {
   const { data } = await api.post(`${BASE}/login`, { email, password });
@@ -17,12 +22,29 @@ export async function login(email: string, password: string) {
     return data;
   }
 
+
+  export async function register(email: string, password: string) {
+    try {
+      const { data } = await api.post(`${BASE}/register`, { username: email, email, password });
+      // Opcional: guardar token si devuelve
+      authStore.set({ token: data.token ?? null, email });
+      return data;
+    }
+    catch (err:any){
+      console.log('REGISTER ERROR =>', err.response?.status,err.response?.data || err.message);
+      throw err;
+    }
+
+  }
+/*
   export async function register(email: string, password: string) {
     const { data } = await api.post(`${BASE}/register`, { username: email, email, password });
     // Opcional: guardar token si devuelve
     authStore.set({ token: data.token ?? null, email });
     return data;
   }
+    */
+
   /*
 export async function register(email: string, password: string) {
   const { data } = await api.post(`${BASE}/register`, { email, password });
